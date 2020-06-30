791871eaf93f04df82a7117fe61e89c0e0c04a88
nicholashanwu <nic2775@hotmail.com>
2020-06-30 23:24:34 +1000

    Changed all expandable cardviews

--- app/src/main/java/com/example/translate/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/translate/ui/home/HomeFragment.java
@@ -21,8 +21,8 @@ import com.makeramen.roundedimageview.RoundedImageView;
 
 public class HomeFragment extends Fragment {
 
-    private RoundedImageView mBtnProfileImage;
-    private FloatingActionButton mFabOneOne;
+	private RoundedImageView mBtnProfileImage;
+	private FloatingActionButton mFabOneOne;
 
 	public View onCreateView(@NonNull LayoutInflater inflater,
 							 ViewGroup container, Bundle savedInstanceState) {
@@ -31,14 +31,10 @@ public class HomeFragment extends Fragment {
 		return root;
 	}
 
-    @Override
+	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 
-        Button mBtnStartNumbers = view.findViewById(R.id.btnStartNumbers);
-        Button mBtnStartGreetings = view.findViewById(R.id.btnStartGreetings);
-        Button mBtnStartFood = view.findViewById(R.id.btnStartFood);
-        Button mBtnStartHelp = view.findViewById(R.id.btnStartHelp);
 		mFabOneOne = view.findViewById(R.id.fabOneOne);
 
 		loadImages(view);
@@ -52,71 +48,11 @@ public class HomeFragment extends Fragment {
 			}
 		});
 
-
-		mBtnStartNumbers.setOnClickListener(new View.OnClickListener() {
-			@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-			@Override
-			public void onClick(View view) {
-
-				Bundle bundle = new Bundle();
-				bundle.putString("learningType", "introduction");
-                Navigation.findNavController(getView()).navigate(R.id.action_navigation_home_to_contentScrollerFragment, bundle);
-			}
-		});
-
-		mBtnStartGreetings.setOnClickListener(new View.OnClickListener() {
-			@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-			@Override
-			public void onClick(View view) {
-
-				Bundle bundle = new Bundle();
-				bundle.putString("learningType", "essentials");
-                Navigation.findNavController(getView()).navigate(R.id.action_navigation_home_to_navigation_learning, bundle);
-			}
-		});
-
-		mBtnStartFood.setOnClickListener(new View.OnClickListener() {
-			@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-			@Override
-			public void onClick(View view) {
-
-				Bundle bundle = new Bundle();
-				bundle.putString("learningType", "food");
-                Navigation.findNavController(getView()).navigate(R.id.action_navigation_home_to_navigation_learning, bundle);
-			}
-		});
-
-		mBtnStartHelp.setOnClickListener(new View.OnClickListener() {
-			@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-			@Override
-			public void onClick(View view) {
-
-				Bundle bundle = new Bundle();
-				bundle.putString("learningType", "help");
-                Navigation.findNavController(getView()).navigate(R.id.action_navigation_home_to_navigation_learning, bundle);
-			}
-		});
-
-		mBtnProfileImage.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Navigation.findNavController(getView()).navigate(R.id.action_navigation_home_to_navigation_profile);
-			}
-		});
-
 	}
 
 	private void loadImages(View view) {
 		mBtnProfileImage = view.findViewById(R.id.btnProfileImageHome);
-		ImageView mIvNumbers = view.findViewById(R.id.ivNumbers);
-		ImageView mIvGreetings = view.findViewById(R.id.ivGreetings);
-		ImageView mIvFood = view.findViewById(R.id.ivFood);
-		ImageView mIvHelp = view.findViewById(R.id.ivHelp);
-		Glide.with(getContext()).load(R.drawable.tzuyu).apply(new RequestOptions().override(100, 100)).into(mBtnProfileImage);
-		Glide.with(getContext()).load(R.drawable.undraw_visual_data).apply(new RequestOptions().override(800, 600).centerCrop()).into(mIvNumbers);
-		Glide.with(getContext()).load(R.drawable.undraw_conversation).apply(new RequestOptions().override(800, 600).centerCrop()).into(mIvGreetings);
-		Glide.with(getContext()).load(R.drawable.undraw_hamburger).apply(new RequestOptions().override(800, 600).centerCrop()).into(mIvFood);
-		Glide.with(getContext()).load(R.drawable.undraw_fatherhood).apply(new RequestOptions().override(800, 600).centerCrop()).into(mIvHelp);
+
 	}
 
 
