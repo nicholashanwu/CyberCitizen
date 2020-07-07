fb234d094fcc5ceb2ccd3deef9772b01050f24f6
nicholashanwu <nic2775@hotmail.com>
2020-07-07 21:22:46 +1000

    Finished CompassFragment Redesigned HomeFragment

--- app/src/main/java/com/example/translate/ui/profile/CompassFragment.java
+++ app/src/main/java/com/example/translate/ui/profile/CompassFragment.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.translate.DatabaseHelper;
 import com.example.translate.R;
@@ -25,6 +26,23 @@ public class CompassFragment extends Fragment {
 
     private AchievementAdapter mAdapter;
     private DatabaseHelper myDb;
+    private CardView mCvAdditionalResources;
+    private CardView mCvGoogle;
+
+    private View.OnClickListener additionalResourcesClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            additionalResourcesButtonClicked();
+        }
+    };
+
+    private View.OnClickListener googleClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            googleButtonClicked();
+        }
+    };
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,9 +53,25 @@ public class CompassFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+        mCvAdditionalResources = view.findViewById(R.id.cvAdditionalResources);
+        mCvGoogle = view.findViewById(R.id.cvGoogle);
+
+
+        mCvAdditionalResources.setOnClickListener(additionalResourcesClickListener);
+        mCvGoogle.setOnClickListener(googleClickListener);
+
         myDb = new DatabaseHelper(getActivity());
 
     }
+
+    private void additionalResourcesButtonClicked() {
+        Navigation.findNavController(getView()).navigate(R.id.action_navigation_compass_to_navigation_resource);
+    }
+
+    private void googleButtonClicked() {
+        Navigation.findNavController(getView()).navigate(R.id.action_navigation_compass_to_navigation_google);
+    }
+
 
     private void showMessage(String title, String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
---
+++ app/src/main/java/com/example/translate/ui/profile/GoogleFragment.java
@@ -0,0 +1,28 @@
+package com.example.translate.ui.profile;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.translate.R;
+
+
+public class GoogleFragment extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_google, container, false);
+	}
+}
